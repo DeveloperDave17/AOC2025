b0e7f4e0a35155154f358286a55e0b1a04303a3a
DeveloperDave17 <DavidHennigan17@gmail.com>
2026-01-27 00:14:09 -0500

    remove target

--- day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/home/david/AOC2025/day2/target/debug/day2: /home/david/AOC2025/day2/src/main.rs
--- day2/target/debug/deps/day2-bf025eeb6202903f.d
+++
@@ -1,5 +0,0 @@
-/home/david/AOC2025/day2/target/debug/deps/libday2-bf025eeb6202903f.rmeta: src/main.rs
-
-/home/david/AOC2025/day2/target/debug/deps/day2-bf025eeb6202903f.d: src/main.rs
-
-src/main.rs:
