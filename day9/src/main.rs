1c21ff17401a5ce0152ae19eefaa6100e740a6a5
DeveloperDave17 <DavidHennigan17@gmail.com>
2026-04-20 22:41:13 -0400

    clean up code

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -13,6 +13,9 @@ struct Edge<'a, 'b> {
     tile_2: &'b Tile
 }
 
+const EPSILON: f64 = 0.0001;
+const TILE_LENGTH: f64 = 1.0;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file = File::open(&args[1]).expect("File not found");
@@ -31,7 +34,7 @@ fn main() {
     for tile in &red_tiles {
         for tile_2 in &red_tiles {
             // difference + 1 to account for the length of a single tile
-            let area = ( (tile.x - tile_2.x).abs() + 1.0 ) * ( ( tile.y - tile_2.y ).abs() + 1.0);
+            let area = ( (tile.x - tile_2.x).abs() + TILE_LENGTH ) * ( ( tile.y - tile_2.y ).abs() + TILE_LENGTH);
 
             if area > largest_area {
                 largest_area = area;
@@ -40,85 +43,6 @@ fn main() {
     }
 
     println!("part one: {}", largest_area);
-
-
-    // Algorithm:
-    // 1. Find all the outer green tiles
-    // 2. Fill in the gaps between green tiles (gets the inner tiles)
-    // 3. Iterate through all the red tile combinations
-    //      a. Determine if the combination's outer edges are comprised of only red or green tiles
-    //      b. If so, Calculate the area between red tiles
-    //      c. replace the largest_area the new area is larger
-
-    // too slow!
-    // use ray casting...
-
-    // let mut green_tiles: HashSet<Tile> = HashSet::new();
-
-    // for i in 0..(red_tiles.len() - 1) {
-    //     let tile_1 = &red_tiles[i];
-    //     let tile_2 = &red_tiles[i + 1];
-    //     if tile_1.x == tile_2.x {
-    //         for y in (tile_1.y)..=tile_2.y {
-    //             green_tiles.insert(Tile {x: tile_1.x, y: y});
-    //         }
-    //     } else {
-    //         for x in (tile_1.x)..=tile_2.x {
-    //             green_tiles.insert(Tile {x: x, y: tile_1.y});
-    //         }
-    //     }
-    // }
-    
-    // // Dont forget the special case where the list wraps
-    // let tile_1 = &red_tiles[0];
-    // let tile_2 = &red_tiles[red_tiles.len() - 1];
-    // if tile_1.x == tile_2.x {
-    //     for y in (tile_1.y)..=tile_2.y {
-    //         green_tiles.insert(Tile {x: tile_1.x, y: y});
-    //     }
-    // } else {
-    //     for x in (tile_1.x)..=tile_2.x {
-    //         green_tiles.insert(Tile {x: x, y: tile_1.y});
-    //     }
-    // }
-
-    // // Red tiles might as well be green tiles too...
-    // for tile in &red_tiles {
-    //     green_tiles.insert(Tile {x: tile.x, y: tile.y});
-    // }
-
-    // // Lets fill in any holes...
-    // let mut inner_tiles: HashSet<Tile> = HashSet::new();
-    // for tile in &green_tiles {
-    //     for tile_2 in &green_tiles {
-    //         if tile.x == tile_2.x {
-    //             if tile_2.y > tile.y {
-    //                 for y in tile.y..=tile_2.y {
-    //                     inner_tiles.insert(Tile{x: tile.x, y: y});
-    //                 }
-    //             } else {
-    //                 for y in tile_2.y..=tile.y {
-    //                     inner_tiles.insert(Tile{x: tile.x, y: y});
-    //                 }
-    //             }
-    //         } else if tile.y == tile_2.y {
-    //             if tile_2.x > tile.x {
-    //                 for x in tile.x..=tile_2.x {
-    //                     inner_tiles.insert(Tile{x: x, y: tile.y});
-    //                 }
-    //             } else {
-    //                 for x in tile_2.x..=tile.x {
-    //                     inner_tiles.insert(Tile{x: x, y: tile.y});
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-
-    // println!("made it this far");
-
-    // // add tiles
-    // green_tiles.extend(inner_tiles);
 
     // reset largest area for part two
     largest_area = 0.0;
@@ -129,21 +53,22 @@ fn main() {
             let mut cloned_tile_1 = Tile {x: tile.x, y: tile.y};
             let mut cloned_tile_2 = Tile {x: tile_2.x, y: tile_2.y};
 
-            // shrink rectangle slightly
+            // shrink area of rectangle slightly
             if cloned_tile_1.x > cloned_tile_2.x {
-                cloned_tile_1.x -= 0.0001;
-                cloned_tile_2.x += 0.0001;
+                cloned_tile_1.x -= EPSILON;
+                cloned_tile_2.x += EPSILON;
             } else {
-                cloned_tile_2.x -= 0.0001;
-                cloned_tile_1.x += 0.0001;
+                cloned_tile_2.x -= EPSILON;
+                cloned_tile_1.x += EPSILON;
             }
 
+            // continue to shrink area of rectangle
             if cloned_tile_1.y > cloned_tile_2.y {
-                cloned_tile_1.y -= 0.0001;
-                cloned_tile_2.y += 0.0001;
+                cloned_tile_1.y -= EPSILON;
+                cloned_tile_2.y += EPSILON;
             } else {
-                cloned_tile_2.y -= 0.0001;
-                cloned_tile_1.y += 0.0001;
+                cloned_tile_2.y -= EPSILON;
+                cloned_tile_1.y += EPSILON;
             }
 
             // opposite corners..
@@ -155,6 +80,7 @@ fn main() {
             let edge_3 = Edge {tile_1: &cloned_tile_1, tile_2: &opp_tile_2};
             let edge_4 = Edge {tile_1: &cloned_tile_2, tile_2: &opp_tile_2};
 
+            // assume rectangle innocence
             let mut within = true;
             for i in 0..(red_tiles.len()) {
                 let vertex_1 = &red_tiles[i];
@@ -165,7 +91,7 @@ fn main() {
 
             if within {
                 // difference + 1 to account for the length of a single tile
-                let area = ( (tile.x - tile_2.x).abs() + 1.0 ) * ( ( tile.y - tile_2.y ).abs() + 1.0);
+                let area = ( (tile.x - tile_2.x).abs() + TILE_LENGTH ) * ( ( tile.y - tile_2.y ).abs() + TILE_LENGTH );
                 if area > largest_area {
                     largest_area = area;
                 }
@@ -176,95 +102,11 @@ fn main() {
     println!("part two: {}", largest_area);
 }
 
-
-// fn determine_if_length_is_valid(value_is_x: bool, lower_value: u64, greater_value: u64, constant_value: u64, green_tiles: &HashSet<Tile>) -> bool {
-//     // assume innocence
-//     let mut length_valid = false;
-
-//     for value in lower_value..=greater_value {
-//         if value_is_x {
-//             length_valid &= green_tiles.contains(&Tile {x: value, y: constant_value});
-//         } else {
-//             length_valid &= green_tiles.contains(&Tile {x: constant_value, y: value});
-//         }
-//     }
-
-//     length_valid
-// }
-
-
-// fn ray_intersects_segment(tile: &Tile, vertex_1: &Tile, vertex_2: &Tile) -> bool {
-//     let x = tile.x as f64;
-//     let mut y = tile.y as f64;
-
-//     let lower_tile =  if vertex_1.y < vertex_2.y { vertex_1 } else { vertex_2 };
-//     let upper_tile = if vertex_1.y > vertex_2.y { vertex_1 } else { vertex_2 }; 
-
-//     if y == lower_tile.y as f64 || y == upper_tile.y as f64 {
-//         y += 0.00001;
-//     }
-
-//     if (y > upper_tile.y as f64 || y < lower_tile.y as f64) || x > lower_tile.x.max(upper_tile.x) as f64 {
-//         false
-//     } else if x < lower_tile.x.min(upper_tile.x) as f64 {
-//         true
-//     } else {
-//         let m_red = if (lower_tile.x - upper_tile.x).abs() as f64 > f64::MIN_POSITIVE {
-//             (upper_tile.y - lower_tile.y) as f64 / (upper_tile.x - lower_tile.x) as f64
-//         } else {
-//             f64::MAX
-//         };
-//         let m_blue = if (lower_tile.x as f64 - x).abs() as f64 > f64::MIN_POSITIVE {
-//             (y - lower_tile.y as f64) / (x - lower_tile.x as f64)
-//         } else {
-//             f64::MAX
-//         };
-//         m_blue >= m_red
-//     }
-
-    // if y == lower_tile.y as f64 || y == upper_tile.y as f64 {
-    //     y += 0.000000001;
-    // }
-
-    // if y < lower_tile.y as f64 || y > upper_tile.y as f64{
-    //     return false;
-    // }
-    // else if x >= max(lower_tile.x, upper_tile.x) as f64 {
-    //     return false;
-    // }
-    // else {
-    //     if x < min(lower_tile.x, upper_tile.x) as f64{
-    //         return true;
-    //     }
-    //     else {
-    //         let mut red = f64::MAX;
-    //         if lower_tile.x != upper_tile.x {
-    //             red = (upper_tile.y as f64 - lower_tile.y as f64) / (upper_tile.x as f64 - lower_tile.x as f64);
-    //         }
-
-    //         let mut blue = f64::MAX;
-    //         if lower_tile.x as f64 != x {
-    //             blue = (y - lower_tile.y as f64) / (x - lower_tile.x as f64);
-    //         }
-
-    //         if blue >= red {
-    //             return true;
-    //         }
-
-    // //         return false;
-    //     }
-    // }
-    
-// }
-
-fn lines_intersect_helper(a: &Tile, b: &Tile, c: &Tile) -> bool{
+fn lines_intersect_helper(a: &Tile, b: &Tile, c: &Tile) -> bool {
     (c.y- a.y) * (b.x-a.x) > (b.y-a.y) * (c.x-a.x)
 }
 
-// fn lines_intersect_helper(a_x: f64, a_y: f64, b_x: f64, b_y: f64, c_x: f64, c_y: f64) -> bool {
-//     (c_y- a_y) * (b_x-a_x) > (b_y-a_y) * (c_x-a_x)
-// }
-
 fn lines_intersect(edge: &Edge, vertex_1: &Tile, vertex_2: &Tile) -> bool {
-    lines_intersect_helper(edge.tile_1, vertex_1, vertex_2) != lines_intersect_helper(edge.tile_2, vertex_1, vertex_2) && lines_intersect_helper(edge.tile_1, edge.tile_2, vertex_1) != lines_intersect_helper(edge.tile_1, edge.tile_2, vertex_2)
+    lines_intersect_helper(edge.tile_1, vertex_1, vertex_2) != lines_intersect_helper(edge.tile_2, vertex_1, vertex_2) 
+    && lines_intersect_helper(edge.tile_1, edge.tile_2, vertex_1) != lines_intersect_helper(edge.tile_1, edge.tile_2, vertex_2)
 }
